42d39dd230fdb904eb620e15fe5533582623a997
Valeria <152978605+lerafml@users.noreply.github.com>
2025-01-16 23:29:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace ParallelApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            CountSpacesInFiles("C:\\Users\\necra\\OneDrive\\Рабочий стол\\test");
+            timer.Stop();
+            Console.WriteLine($"Время выполнения в мс: {timer.Elapsed.TotalMilliseconds}");
+        }
+
+        public static void CountSpacesInFiles(string path)
+        {
+            List<Task> tasks = new List<Task>();
+            foreach(var file in Directory.GetFiles(path))
+            {
+                tasks.Add(Task.Run(() => DoCount(file)));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        public static void DoCount(string file)
+        {
+            string text = File.ReadAllText(file);
+            int count = 0;
+            foreach(char c in text)
+            {
+                if (c == ' ')
+                    count++;
+            }
+            Console.WriteLine($"Количество пробелов: {count} в файле: {file}");
+        }
+    }
+}
